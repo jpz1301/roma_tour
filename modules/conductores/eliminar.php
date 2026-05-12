3031f75ccc114e2eef7c7734136fbaff38b6b74a
jpz1301 <santiago.mesicano@gmail.com>
2026-05-12 12:02:27 -0500

    actualizacion general

--- modules/conductores/eliminar.php
+++ modules/conductores/eliminar.php
@@ -1,14 +1,13 @@
 <?php
+include("../../includes/seguridad.php");
 include("../../config/conexion.php");
-
 
 $id = intval($_GET['id']);
 
-// 1. Borrar registros relacionados
-pg_query($conexion, "DELETE FROM inspecciones_vehiculo WHERE id_conductor = $id");
-
-// 2. Ahora sí borrar conductor
-pg_query($conexion, "DELETE FROM conductores WHERE id_conductor = $id");
+if ($id > 0) {
+    // Eliminar el conductor directamente (sin pasar por inspecciones)
+    pg_query_params($conexion, "DELETE FROM conductores WHERE id_conductor = $1", [$id]);
+}
 
 header("Location: conductores.php");
 exit();
